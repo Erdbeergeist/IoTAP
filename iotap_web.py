2b1f6679072f6f2b426c5c0c44e50313fd1f4e61
Lukas Mittnacht <luxmitges@gmail.com>
2024-12-26 14:04:38 +0100

    bugfixes

--- iotap_web.py
+++ iotap_web.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, render_template
+import time
 import threading
 import subprocess
 import logging
 
 app = Flask(__name__)
 shutdown_timer = None
-timeout = 1200
+timeout = 1200 
+shutdown_expiration = None
 
 AP_NAME = "iotap"
 logging.basicConfig(filename='ap_control.log', level=logging.DEBUG)
@@ -13,6 +15,7 @@ logging.basicConfig(filename='ap_control.log', level=logging.DEBUG)
 def disable_wifi():
     """Disable wifi"""
     shutdown_timer = None
+    shutdown_expiration = None
     subprocess.run(["sudo", "nmcli", "connection", "down", AP_NAME])
 
 
@@ -30,14 +33,19 @@ def get_ap_status():
 
 def toggle_ap():
     """Toggle the access point state."""
+    global shutdown_timer, shutdown_expiration
     if get_ap_status():
+        shutdown_expiration = None
         if shutdown_timer:
             shutdown_timer.cancel()
-        shutdown_timer = threading.Timer(timeout, disable_wifi)
-        shutdown_timer.start()
         subprocess.run(["sudo", "nmcli", "connection", "down", AP_NAME])
         return False
     else:
+        if shutdown_timer:
+            shutdown_timer.cancel()
+        shutdown_timer = threading.Timer(timeout, disable_wifi)
+        shutdown_expiration = time.time() + timeout
+        shutdown_timer.start()
         subprocess.run(["sudo", "nmcli", "connection", "up", AP_NAME])
         return True
 
@@ -57,6 +65,15 @@ def toggle():
     """API endpoint to toggle AP status."""
     new_status = toggle_ap()
     return jsonify({"ap_active": new_status})
+
+@app.route("/remaining_time", methods=["GET"])
+def get_remaining_time():
+    global shutdown_expiration
+    if get_ap_status() and shutdown_expiration:
+        remaining_time = max(0, int(shutdown_expiration - time.time()))
+        return jsonify({"remaining_time": remaining_time}), 200
+    else:
+        return jsonify({"remaining_time": 0}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
